92bf91b0de7c726bdb9e7443eba05285bdcbb1f5
Patrick Miller <patrickm885@gmail.com>
2018-04-22 17:19:06 -0500

    feat: routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,7 +8,6 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
   heroes: Hero[];
 
   constructor(private heroService: HeroService) { }
@@ -16,10 +15,6 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero) {
-    this.selectedHero = hero;
   }
 
   ngOnInit() {
